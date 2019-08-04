1d22b48a77bb17dcd9ec74b47561770d2b1434d9
Fernando Broqua <ferbroqua@gmail.com>
2019-08-04 05:22:45 -0300

    Especificado de campos query + funcion comentada

--- login.php
+++ login.php
@@ -10,13 +10,13 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     $password = $_POST["password"];
     $access = "true";
     
-    $statement = mysqli_prepare($con, "SELECT * FROM usuarios WHERE username = ? AND password = ? AND access = ?");
+    $statement = mysqli_prepare($con, "SELECT userID as id, name, surname, username, password, access 
+                                    FROM usuarios WHERE username = ? AND password = ? AND access = ?");
     mysqli_stmt_bind_param($statement, "sss", $username, $password, $access);
     mysqli_stmt_execute($statement);
     
-    mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $name, $surname, $username, $password, $access);
-    
+    mysqli_stmt_bind_result($statement, $id, $name, $surname, $username, $password, $access);
+    //mysqli_stmt_store_result($statement);
     
     while(mysqli_stmt_fetch($statement)){
         $response["succes"] = true;  
